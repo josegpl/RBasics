997ba3176a097304fa4a1d4b7c6ef63922face94
José Guilherme <guilherme-lima@hotmail.com>
2020-03-21 00:16:26 -0300

    more data manipulation

--- firstRscript.r
+++ firstRscript.r
@@ -32,3 +32,29 @@ names(movielens)
 
 levels(movielens$genres)
 nlevels(movielens$genres)
+
+#more manipulation: attach only the number of mrders to the state
+#and sort by less murders to most murders
+
+sort(murders$total)
+
+a <- murders$total
+a
+b<-murders$state
+b
+
+  #visualize and sort 
+names(a) <- b
+a
+
+sort(a)
+
+#doing the same above using only order fuction and index variable
+#except showing the total numbers
+
+index <- order(murders$total)
+murders$state[index]
+
+#we can also do that by retrieving the maximum or mininum numbers
+  #using max function or by using which.max function which
+    #returns THE INDEX of where that max or min is
