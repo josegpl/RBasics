bd96291c5150b893f2cedbbf2b2e6fbe8d6a9b51
José Guilherme <guilherme-lima@hotmail.com>
2020-03-24 20:00:14 -0300

    which(), match(), and %in% operator manipulations

--- indexing.R
+++ indexing.R
@@ -19,3 +19,29 @@ safe <- murder_rate <= 1
 # defining an index and identifying states with both conditions true
 index <- safe & west
 murders$state[index]
+
+#which() function which returns the index of instances in a vector that are TRUE
+
+vector <- c(TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE)
+country <- c("GERMANY", "FRANCE", "PORTUGAL", "ITALY", "ENGLAND", "IRELAND", "NETHERLANDS", "BELGIUM")
+names(vector) <- country
+x <- which(vector)
+vector[x]
+
+# to determine the murder rate in Massachusetts we may do the following
+index <- which(murders$state == "Massachusetts")
+index
+murder_rate[index]
+
+# to obtain the indices and subsequent murder rates of New York, Florida, Texas, we do:
+index <- match(c("New York", "Florida", "Texas"), murders$state)
+index
+murders$state[index]
+murder_rate[index]
+
+x <- c("a", "b", "c", "d", "e")
+y <- c("a", "d", "f")
+y %in% x
+
+# to see if Boston, Dakota, and Washington are states
+c("Boston", "Dakota", "Washington") %in% murders$state
